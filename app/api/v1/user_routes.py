ce9a3168fc0bf074a9fa47b62a975ef3924d0e5b
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-04-22 23:28:05 -0500

    Update user_routes.py

--- app/api/v1/user_routes.py
+++ app/api/v1/user_routes.py
@@ -28,7 +28,7 @@ def login(data: UserLogin, db: Session = Depends(get_db)):
     token = create_access_token({
         "sub": str(db_user.id),
         "username": db_user.username,
-        "role_id": db_user.role_id
+        "role_id": str(db_user.role_id)
     })
 
     return {"access_token": token, "token_type": "bearer"}
