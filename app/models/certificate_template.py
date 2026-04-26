4a44b9d5660bf800fd52eb5a0c2191081de671ae
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-04-25 21:54:46 -0500

    Update certificate_template.py

--- app/models/certificate_template.py
+++ app/models/certificate_template.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, ForeignKey, DateTime, String, Boolean
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql import func
-from app.db.base_class import Base
+from app.db.base import Base
 
 class CertificateTemplate(Base):
     __tablename__ = "certificate_templates"
