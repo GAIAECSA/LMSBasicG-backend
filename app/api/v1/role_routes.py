a57cdef64556947aa718a0801842f1d223b266a5
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-04-27 09:08:15 -0500

    Update role_routes.py

--- app/api/v1/role_routes.py
+++ app/api/v1/role_routes.py
@@ -7,16 +7,16 @@ from app.services import role_service
 
 router = APIRouter()
 
-@router.post("/roles", response_model=RoleResponse)
-def create_role(data: RoleCreate, db: Session = Depends(get_db)):
-    try:
-        return role_service.create_role(db, data)
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+#@router.post("/roles", response_model=RoleResponse)
+#def create_role(data: RoleCreate, db: Session = Depends(get_db)):
+    #try:
+        #return role_service.create_role(db, data)
+    #except Exception as e:
+        #raise HTTPException(status_code=400, detail=str(e))
     
-@router.get("/roles/{role_id}", response_model=RoleResponse)
-def get_role(role_id: int, db: Session = Depends(get_db)):
-    role = role_service.get_role_by_id(db, role_id)
-    if not role:
-        raise HTTPException(status_code=404, detail="Rol no encontrado")
-    return role
+#@router.get("/roles/{role_id}", response_model=RoleResponse)
+#def get_role(role_id: int, db: Session = Depends(get_db)):
+    #role = role_service.get_role_by_id(db, role_id)
+    #if not role:
+        #raise HTTPException(status_code=404, detail="Rol no encontrado")
+    #return role
