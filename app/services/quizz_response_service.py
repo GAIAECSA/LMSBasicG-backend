c95c3477434586cb8c25b8fad748e5ecd5a7a528
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-05-15 22:32:45 -0500

    Update quizz_response_service.py

--- app/services/quizz_response_service.py
+++ app/services/quizz_response_service.py
@@ -2,6 +2,7 @@ from sqlalchemy.orm import Session
 from app.models.quizz_response import QuizzResponse
 from app.repositories import quizz_response_repo
 from app.schemas.quizz_response import QuizzResponseCreate, QuizzResponseUpdate
+from app.services.lesson_block_service import get_lesson_block
 
 def create_quizz_response(db: Session, data: QuizzResponseCreate):
     existing = quizz_response_repo.get_by_enrollment_and_lesson_block(db,data.enrollment_id,data.lesson_block_id)
@@ -36,7 +37,20 @@ def get_quizz_response(db: Session, quizz_response_id: int):
     return quizz_response
 
 def get_by_enrollment(db: Session, enrollment_id: int):
-    return quizz_response_repo.get_all_by_enrollment(db, enrollment_id)
+    quizzes = quizz_response_repo.get_all_by_enrollment(
+        db,
+        enrollment_id
+    )
+    return [
+        quizz
+        for quizz in quizzes
+        if (
+            lesson_block := get_lesson_block(
+                db,
+                quizz.lesson_block_id
+            )
+        ) and lesson_block.is_required
+    ]
 
 def get_by_block(db: Session, lesson_block_id: int):
     return quizz_response_repo.get_all_by_lesson_block(db, lesson_block_id)
