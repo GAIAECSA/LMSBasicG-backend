85144c805a29937ffbb14db827492161d9f8d88f
Sebastian Ilbay <atlantiscityplay@gmail.com>
2026-05-24 21:26:58 -0500

    Update course_service.py

--- app/services/course_service.py
+++ app/services/course_service.py
@@ -1,3 +1,5 @@
+from sympy import true
+
 from app.models.lesson_block import LessonBlock
 from sqlalchemy.orm import Session
 from fastapi import UploadFile
@@ -16,6 +18,7 @@ def create_course(
     data: CourseCreate,
     image: UploadFile | None = None,
 ):
+
     with db.begin():
 
         existing = course_repo.get_by_name_and_subcategory(
@@ -25,15 +28,9 @@ def create_course(
         )
 
         if existing:
-            raise ValueError(
-                "El curso ya existe en esta subcategoría"
-            )
+            raise ValueError("El curso ya existe en esta subcategoría")
 
-        image_url = (
-            save_course_image(image)
-            if image
-            else None
-        )
+        image_url = save_course_image(image) if image else None
 
         course = Course(
             **data.model_dump(),
@@ -42,33 +39,9 @@ def create_course(
 
         course_repo.create(db, course)
 
-        blocks = [
-            build_lesson_block(
-                {
-                    "type": "introduction",
-                    "text": "Bienvenido al curso!",
-                },
-                0,
-            ),
-            build_lesson_block(
-                {
-                    "type": "video",
-                    "url": "https://...",
-                },
-                1,
-            ),
-            build_lesson_block(
-                {
-                    "type": "quiz",
-                    "questions": [],
-                },
-                2,
-            ),
-        ]
-
         lesson_block_repo.create_all(
             db,
-            blocks,
+            create_default_blocks(),
         )
 
         return course
@@ -127,21 +100,99 @@ def get_all_courses(db: Session):
     return course_repo.get_all(db)
 
 
+# Privados
+DEFAULT_MDT_BLOCKS = [
+    {
+        "content": {
+            "title": "Copia Cédula",
+            "instructions": "Subir la copia de cédula",
+            "attachments": [],
+        },
+        "order": 0,
+        "completion_type": "SUBIR",
+        "is_active": True,
+    },
+    {
+        "content": {
+            "title": "Copia Título",
+            "instructions": "Subir la copia de título",
+            "attachments": [],
+        },
+        "order": 1,
+        "completion_type": "SUBIR",
+        "is_active": True,
+    },
+    {
+        "content": {
+            "title": "Copia de Certificado Laboral",
+            "instructions": "Subir la copia de certificado laboral",
+            "attachments": [],
+        },
+        "order": 2,
+        "completion_type": "SUBIR",
+        "is_active": True,
+    },
+    {
+        "content": {
+            "title": "Comprobante de pago",
+            "instructions": "Subir el comprobante de pago",
+            "attachments": [],
+        },
+        "order": 3,
+        "completion_type": "SUBIR",
+        "is_active": True,
+    },
+    {
+        "content": {
+            "title": "Informe del Instructor",
+            "instructions": "Subir el informe del instructor",
+            "attachments": [],
+        },
+        "order": 4,
+        "completion_type": "VER",
+        "is_active": False,
+    },
+    {
+        "content": {
+            "title": "Evidencia asistencia",
+            "instructions": "Evidencia asistencia",
+            "attachments": [],
+        },
+        "order": 5,
+        "completion_type": "VER",
+        "is_active": False,
+    },
+]
+
+
+def create_default_blocks():
+
+    return [
+        build_lesson_block(
+            content=block["content"],
+            order=block["order"],
+            completion_type=block["completion_type"],
+            is_active=block["is_active"],
+        )
+        for block in DEFAULT_MDT_BLOCKS
+    ]
 
 
 def build_lesson_block(
     content: dict,
     order: int,
+    completion_type: str,
+    is_active: bool,
 ):
     return LessonBlock(
         content=content,
-        completion_type="automatic",
+        completion_type=completion_type,
         completion_value=0,
         order=order,
         default=True,
         lesson_id=None,
         block_type_id=1,
         date_available=None,
-        is_active=True,
+        is_active=is_active,
         deleted=False,
     )
